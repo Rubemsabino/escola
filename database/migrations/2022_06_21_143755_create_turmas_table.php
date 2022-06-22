c11b6dd57e26ea373cb98f6fe9e973cde528d591
Rubem Juniore <Rubemsabinolibras@gmail.com>
2022-06-22 10:29:23 -0300

    commit inicial

---
+++ database/migrations/2022_06_21_143755_create_turmas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('turmas', function (Blueprint $table) {
+            $table->id();
+            $table-> string('nome');
+        //    $table-> foreignId('professores_id')->constrained();
+        $table->unsignedBigInteger('id_professor');
+        $table->foreign('id_professor')->references('id')->on('professores');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('turmas');
+    }
+};
